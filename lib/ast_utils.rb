21b90a25426ba984edbf303d0009a26eca4bb71e
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-02-26 20:37:07 +0900

    Delete NodeSet

--- lib/ast_utils.rb
+++ lib/ast_utils.rb
@@ -3,7 +3,6 @@ require "ast_utils/version"
 require "pathname"
 require "set"
 
-require "ast_utils/node_set"
 require "ast_utils/node_helper"
 require "ast_utils/partial_map"
 require "ast_utils/labeling"
